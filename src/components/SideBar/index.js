756cc5ad9133fc92a6f6e87a17b2fb138e520ecb
iqbal125 <iqbal125@yahoo.com>
2020-08-23 17:55:25 -0700

    fix image

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,23 +1,107 @@
-import React, { useEffect, useContext, useRef } from 'react';
+import React, { useState } from 'react';
 import styles from './sidebar.module.css';
 import logo from '../../../static/logos/favicon.ico';
 import { navigate } from 'gatsby';
 
 const SideBar = ({ props }) => {
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const accordianHandler = () => (show ? setShow(false) : setShow(true));
+
+  const accordianHandler2 = () => (show2 ? setShow2(false) : setShow2(true));
+
   return (
     <div className={styles.side_drawer_full}>
       <div className={styles.app_logo} onClick={() => navigate('/app/dashboard')}>
         <img src={logo} alt="" />
       </div>
-      <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
-        Create Task
+
+      <div style={{ color: 'white' }} onClick={accordianHandler}>
+        Show Links
       </div>
-      <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
-        List Tasks
+      {show && (
+        <div>
+          <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
+            Create Task
+          </div>
+          <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
+            List Tasks
+          </div>
+        </div>
+      )}
+
+      <div style={{ color: 'white' }} onClick={accordianHandler}>
+        Show Links
       </div>
-      <div className={styles.side_items} onClick={() => navigate('/app/task')}>
+      {show && (
+        <div>
+          <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
+            Create Task
+          </div>
+          <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
+            List Tasks
+          </div>
+        </div>
+      )}
+
+      <div style={{ color: 'white' }} onClick={accordianHandler}>
+        Show Links
+      </div>
+      {show && (
+        <div>
+          <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
+            Create Task
+          </div>
+          <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
+            List Tasks
+          </div>
+        </div>
+      )}
+
+      <div style={{ color: 'white' }} onClick={accordianHandler}>
+        Show Links
+      </div>
+      {show && (
+        <div>
+          <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
+            Create Task
+          </div>
+          <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
+            List Tasks
+          </div>
+        </div>
+      )}
+
+      <div style={{ color: 'white' }} onClick={accordianHandler}>
+        Show Links
+      </div>
+      {show && (
+        <div>
+          <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
+            Create Task
+          </div>
+          <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
+            List Tasks
+          </div>
+        </div>
+      )}
+      <div style={{ color: 'white' }} onClick={accordianHandler}>
+        Show Links
+      </div>
+      {show && (
+        <div>
+          <div className={styles.side_items} onClick={() => navigate('/app/createtask')}>
+            Create Task
+          </div>
+          <div className={styles.side_items} onClick={() => navigate('/app/listtasks')}>
+            List Tasks
+          </div>
+        </div>
+      )}
+
+      {/*<div className={styles.side_items_settings} onClick={() => navigate('/app/task')}>
         Settings
-      </div>
+      </div>*/}
     </div>
   );
 };
