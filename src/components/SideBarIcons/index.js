ab0de9d735d8a726c6c8aba060e6c43b2fc26d16
iqbal125 <iqbal125@yahoo.com>
2020-08-24 19:34:40 -0700

    added side bar styling

--- src/components/SideBarIcons/index.js
+++ src/components/SideBarIcons/index.js
@@ -3,7 +3,6 @@ import styles from './sidebaricons.module.css';
 import logo from '../../../static/logos/favicon.ico';
 import { navigate } from 'gatsby';
 import { VscSettingsGear } from 'react-icons/vsc';
-
 import IconMenu from '../IconMenu';
 
 const SideBarIcons = props => {
