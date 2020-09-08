b67cc4676a27a475bf70bcdba6417e18253b33be
iqbal125 <iqbal125@yahoo.com>
2020-09-07 18:50:47 -0700

    fixx mouse over event

--- src/components/IconMenu/index.js
+++ src/components/IconMenu/index.js
@@ -7,23 +7,12 @@ const IconMenu = props => {
   const { id, icon, header, url } = AppLink;
   const ref = useRef(null);
 
-  useEffect(() => {
-    document.addEventListener('mouseover', handleClickOutside);
-    return () => {
-      document.removeEventListener('mouseover', handleClickOutside);
-    };
-  });
-
-  const handleClickOutside = event => {
-    if (ref.current && !ref.current.contains(event.target)) {
-      if (openID === id) {
-        setOpenID(null);
-      }
-    }
-  };
-
   return (
-    <div onMouseOver={() => setOpenID(id)} className={styles.container}>
+    <div
+      onMouseOver={() => setOpenID(id)}
+      onMouseLeave={() => setOpenID(null)}
+      className={styles.container}
+    >
       <div className={styles.icon_container}>
         <Link to={url} className={styles.icons} activeClassName={styles.icon_active}>
           {icon()}
